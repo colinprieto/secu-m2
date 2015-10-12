8f9fe7ca05c27a71931f13fc0c64cbdbec0808ec
iabalea <isabelleabalea@yahoo.fr>
2015-10-12 10:24:11 +0200

    Changement répertoire

--- spread/connexion.sh
+++ spread/connexion.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-ssh etu@IP
-ssh-keygen -t rsa
-ssh-copy-id @IP
-
+PASS="-stri-" 
 
 
 # si ça marche envoi du fichier
 sshpass -p -etu- ssh etu@$1
 scp /chemin1/ "etu@"$1:/chemin2
 
-# connexion ssh
-ssh "etu@"$1 
-
 #connexion en root
 su -
+echo $PASS
 
 #déplace le fichier
 mv 
