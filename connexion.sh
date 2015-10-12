302912f2378e0b3e7bd47ae8141ca9fa4d59f959
iabalea <isabelleabalea@yahoo.fr>
2015-10-12 10:06:07 +0200

    Ajout script connexion

---
+++ connexion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ssh etu@IP
+ssh-keygen -t rsa
+ssh-copy-id @IP
+
+
+
+# si ça marche envoi du fichier
+sshpass -p -etu- ssh etu@$1
+scp /chemin1/ "etu@"$1:/chemin2
+
+# connexion ssh
+ssh "etu@"$1 
+
+#connexion en root
+su -
+
+#déplace le fichier
+mv 
+
+#exécute le fichier
+./french&
